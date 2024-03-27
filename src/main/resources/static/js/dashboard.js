95a6476d36cc724c69c1d016157adf22cd173959
leeChanSin <ebdh56@naver.com>
2024-03-27 14:10:23 +0900

    [add] payment Test Data

--- src/main/resources/static/js/dashboard.js
+++ src/main/resources/static/js/dashboard.js
@@ -1,110 +1,237 @@
-var today = new Date();
-    var currentMonth = today.getMonth(); // 현재 월 (0부터 시작)
-    var currentYear = today.getFullYear(); // 현재 년도
 
-    // 해당 월의 첫 번째 날짜를 가져옵니다.
-    var firstDayOfMonth = new Date(currentYear, currentMonth, 1);
 
-    // 해당 월의 마지막 날짜를 가져옵니다.
-    var lastDayOfMonth = new Date(currentYear, currentMonth + 1, 0);
-    var totalDays = lastDayOfMonth.getDate(); // 해당 월의 총 일수
 
-    var categories = [];
-    for (var i = 1; i <= totalDays; i++) {
-    categories.push(i);
-    }
 
-    var options = {
-      series: [{
-      name: 'Inflation', // 마우스 올렸을때 컬럼 이름
-      data: [30, 20, 15, 10, 14, 14, 6, 25, 22, 15, 17, 26, 20, 11, 27, 24, 22, 24, 9, 25, 17, 15, 11, 21, 25, 16, 25, 16, 14, 20 ,30] // 날짜별 결제 내역갯수
+
+
+
+
+// 탭을 변경할 때마다 해당 탭에 맞는 카테고리 생성
+function monthYearChange(tagName) {
+  var today = new Date();
+  var categories = [];
+  if (tagName === 'chartMonth') {
+      var currentMonth = today.getMonth(); // 현재 월 (0부터 시작)
+      var currentYear = today.getFullYear(); // 현재 년도
+
+      // 해당 월의 첫 번째 날짜를 가져옵니다.
+      var firstDayOfMonth = new Date(currentYear, currentMonth, 1);
+
+      // 해당 월의 마지막 날짜를 가져옵니다.
+      var lastDayOfMonth = new Date(currentYear, currentMonth + 1, 0);
+      var totalDays = lastDayOfMonth.getDate(); // 해당 월의 총 일수
+
+      for (var i = 1; i <= totalDays; i++) {
+          categories.push(i);
+      }
+      return categories;
+  } else if (tagName === 'chartYear') {
+      for (var i = 1; i <= 12; i++) {
+          categories.push(i);
+      }
+      return categories;
+  }
+
+}
+
+
+
+let categoriesMonth = monthYearChange('chartMonth');
+
+// 이번달 차트
+var optionMonth = {
+    series: [{
+
+        data: [30, 20, 15, 10, 14, 14, 6, 25, 22, 15, 17, 26, 20, 11, 27, 24, 22, 24, 9, 25, 17, 15, 11, 21, 25, 16, 25, 16, 14, 20, 30] // 날짜별 결제 내역갯수
     }],
-      chart: { // 차트 전체 크기
+    chart: { // 차트 전체 크기
         height: '80%',
         width: '100%',
         type: 'bar',
+        toolbar: {
+            show: false, // 다운로드 버튼 숨기기
+        },
+
     },
     plotOptions: {
-      bar: {
-        //borderRadius: 10,
-        columnWidth: '15%', // bar 너비 조절
-        dataLabels: {
-          position: 'top', // top, center, bottom
-        },
-      }
+        bar: {
+            columnWidth: '15%', // bar 너비 조절
+
+        }
     },
     dataLabels: {
-      enabled: false, // 컬럼바 위 텍스트 표시x
-      offsetY: -20,
-      style: {
-        fontSize: '12px', // bar의 폰트 크기
-        colors: ["#304758"]
-      }
+        enabled: false, // 컬럼바 위 텍스트 표시x
+
+        style: {
+            fontSize: '12px', // bar의 폰트 크기
+            colors: ["#304758"]
+        }
     },
     tooltip: {
-      enabled: true, // 툴팁 활성화
-      custom: function({ series, seriesIndex, dataPointIndex, w }) {
-        // 툴팁 커스텀 내용을 생성합니다.
-        return '<div class="custom-tooltip">' +
-          '<span>' + series[seriesIndex][dataPointIndex] + '</span>' +
-          '</div>';
-      }
+        enabled: true, // 툴팁 활성화
+        custom: function({
+            series,
+            seriesIndex,
+            dataPointIndex,
+            w
+        }) {
+            // 툴팁 커스텀 내용을 생성합니다.
+            return '<div class="custom-tooltip">' +
+                '<span>' + series[seriesIndex][dataPointIndex] + '</span>' +
+                '</div>';
+        }
     },
     xaxis: {
-      //categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-      categories: categories,
-      position: 'bottom',
-      axisBorder: {
-        show: false
-      },
-      axisTicks: {
-        show: false
-      },
-      crosshairs: {
-        fill: {
-          type: 'gradient',
-          gradient: {
-            colorFrom: '#D8E3F0',
-            colorTo: '#BED1E6',
-            stops: [0, 100],
-            opacityFrom: 0.4,
-            opacityTo: 0.5,
-          }
-        }
-      },
-
+        categories: categoriesMonth,
+        position: 'bottom',
+        labels: {
+            show: true,
+        },
+        axisBorder: {
+            show: false,
+        },
+        axisTicks: {
+            show: false,
+        },
     },
     yaxis: {
-      axisBorder: {
-        show: false
-      },
-      axisTicks: {
-        show: false,
-      },
-      labels: {
-        show: false,
-        formatter: function (val) {
-          return val + "%";
+        show: true,
+        opposite: true, // y축을 오른쪽에 배치합니다.
+        forceNiceScale: true,
+        labels: {
+            show: true,
+            formatter: function(val) {
+                return val;
+            }
+
+        },
+
+
+
+
+    },
+
+  };
+
+
+  var chartMonth = new ApexCharts(document.querySelector("#chartMonth"), optionMonth);
+  chartMonth.render();
+
+
+
+    let categoriesYear = monthYearChange('chartYear');
+    // 년도 기준 월별 차트
+    var optionYear = {
+        series: [{
+
+            data: [30, 20, 15, 10, 14, 14, 6, 25, 22, 15, 17, 26] // 날짜별 결제 내역갯수
+        }],
+        chart: { // 차트 전체 크기
+            height: '80%',
+            width: '100%',
+            type: 'bar',
+            toolbar: {
+                show: false, // 다운로드 버튼 숨기기
+            },
+
+        },
+        plotOptions: {
+            bar: {
+                columnWidth: '15%', // bar 너비 조절
+
+            }
+        },
+        dataLabels: {
+            enabled: false, // 컬럼바 위 텍스트 표시x
+
+            style: {
+                fontSize: '12px', // bar의 폰트 크기
+                colors: ["#304758"]
+            }
+        },
+        tooltip: {
+            enabled: true, // 툴팁 활성화
+            custom: function({
+                series,
+                seriesIndex,
+                dataPointIndex,
+                w
+            }) {
+                // 툴팁 커스텀 내용을 생성합니다.
+                return '<div class="custom-tooltip">' +
+                    '<span>' + series[seriesIndex][dataPointIndex] + '</span>' +
+                    '</div>';
+            }
+        },
+        xaxis: {
+            categories: categoriesYear,
+            position: 'bottom',
+            labels: {
+                show: true,
+            },
+            axisBorder: {
+                show: false,
+            },
+            axisTicks: {
+                show: false,
+            },
+        },
+        yaxis: {
+            show: true,
+            opposite: true, // y축을 오른쪽에 배치합니다.
+            forceNiceScale: true,
+            labels: {
+                show: true,
+                formatter: function(val) {
+                    return val;
+                }
+
+            },
+
+
+
+
+        },
+
+      };
+
+    var chartYear = new ApexCharts(document.querySelector("#chartYear"), optionYear);
+    chartYear.render();
+
+
+
+
+    function openTab(tabName) {
+        var i, tabContent, tabBtns;
+        let month = document.getElementById("monthChart");
+        let year = document.getElementById("yearChart");
+        console.log("month",month.value);
+        console.log("year",year.value);
+        // 모든 탭 내용과 탭 버튼을 숨김
+        tabContent = document.getElementsByClassName('tab-content');
+        for (i = 0; i < tabContent.length; i++) {
+            tabContent[i].style.display = 'none';
         }
-      }
 
-    },
-    title: {
-      //text: '결제 처리 통계', // 차트 밑 텍스트
-      floating: true,
-      offsetY: -10,
-      offsetX: 30,
-      align: 'left',
-      style: {
-        // <color></color>: '#444'
-      }
-    },
-    legend: {
-    position: 'right', // 범례 위치 설정
-    offsetY: 40, // 범례의 Y축 오프셋 설정
+        tabBtns = document.getElementsByClassName('tab-btn');
+        for (i = 0; i < tabBtns.length; i++) {
+            tabBtns[i].classList.remove('active');
+        }
+
+        // 선택된 탭을 보이게 함
+        document.getElementById(tabName).style.display = 'block';
+        console.log(tabName);
+        // 선택된 탭의 차트를 그림
+        if (tabName == 'chartMonth') {
+            month.style.borderBottom = '1px solid blue';
+            year.style.borderBottom = ''; // 다른 탭의 스타일 제거
+            // chartMonth.render();
+
+        } else if (tabName == 'chartYear') {
+            year.style.borderBottom = '1px solid blue';
+            month.style.borderBottom = ''; // 다른 탭의 스타일 제거
+            // chartYear.render();
+        }
     }
-    };
-
-var chart = new ApexCharts(document.querySelector("#chart"), options);
-chart.render();
-
+    // 초기에 첫 번째 탭을 열도록 함
+    openTab('chartMonth');
